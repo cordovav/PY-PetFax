faf3b39cf1b57ea7dcda96a942be6d7df8d37e4d
Veny Cordova <venycordova@gmail.com>
2023-04-01 13:51:44 -0400

    adding data to a database

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,8 +1,19 @@
 from flask import Flask 
+from flask_migrate import Migrate
+
 #this would be considered our factory method
 def create_app(): 
     app = Flask(__name__)
 
+    #database config
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+    from . import models
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
+    
     @app.route('/')
     def hello(): 
         return 'Hello, PetFax!'
@@ -16,3 +27,4 @@ def create_app():
     app.register_blueprint(fact.bp)
     
     return app
+
